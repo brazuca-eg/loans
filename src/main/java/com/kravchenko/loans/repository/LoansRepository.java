fdfdb75ad34834e4ee5bff043ac3e7663fbd0145
Yehor Kravchenko <brazuca.egor@gmail.com>
2024-01-23 22:46:01 +0200

    DEV: Initial commit

---
+++ src/main/java/com/kravchenko/loans/repository/LoansRepository.java
@@ -0,0 +1,16 @@
+package com.kravchenko.loans.repository;
+
+import com.kravchenko.loans.entity.Loans;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LoansRepository extends JpaRepository<Loans, Long> {
+
+    Optional<Loans> findByMobileNumber(String mobileNumber);
+
+    Optional<Loans> findByLoanNumber(String loanNumber);
+
+}
